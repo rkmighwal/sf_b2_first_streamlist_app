3da1a6a40f9aa37149ea43488fbf627b6bc19cbe
Rohit Mighwal <rkmighwal@gmail.com>
2023-03-03 03:10:44 +0530

    Added dummy code

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,3 +45,6 @@ streamlit.dataframe(my_data_rows)
 # add fruit
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
 streamlit.write('Thanks for adding ', add_my_fruit)
+
+# dummy code
+my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ('from streamlit')")
