90704dd41bd621a854b5f5831880e2850fbbab0f
Rohit Mighwal <rkmighwal@gmail.com>
2023-03-03 03:08:04 +0530

    Add new section

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,3 +41,7 @@ my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The Fruit list contains:")
 streamlit.dataframe(my_data_rows)
+
+# add fruit
+add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
+streamlit.write('Thanks for adding ', add_my_fruit)
